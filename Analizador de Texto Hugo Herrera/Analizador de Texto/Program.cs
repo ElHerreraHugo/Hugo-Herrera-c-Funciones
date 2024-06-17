f032aadb32c1ce115f3d4dd0e9e5c378d3d00eb0
ElHerreraHugo <167731983+ElHerreraHugo@users.noreply.github.com>
2024-06-17 18:10:20 -0400

    commit 1

---
+++ Analizador de Texto Hugo Herrera/Analizador de Texto/Program.cs
@@ -0,0 +1,53 @@
+﻿
+class Program
+{
+ 
+    static void Main(string par)
+    {
+        Console.WriteLine(" Ingrese un Parrafo o un Texto : ");
+
+
+        string texto = Console.ReadLine();
+   
+
+        int cantidadPalabras = ContarPalabras(texto);
+        int cantidadVocales = ContarVocales(texto);
+        string textoInvertido = InvertirTexto(texto);
+
+
+        Console.WriteLine($"Número de palabras: {cantidadPalabras}");
+                Console.WriteLine($"Número de vocales: {cantidadVocales}");
+                Console.WriteLine($"Texto invertido: {textoInvertido}");
+            }
+
+    static int ContarPalabras(string texto)
+
+    {
+        string[] palabras = texto.Split(' ');
+        return palabras.Length;
+    }
+   
+    static int ContarVocales(string texto)
+    {
+        int contar = 0;
+        foreach (char cortar in texto.ToLower())
+        {
+            if ("a e i o u".Contains(cortar))
+            {
+                contar++;
+            }
+        }
+        return contar;
+    }
+    static string InvertirTexto(string texto)
+    {
+        char[] cadena = texto.ToCharArray();
+        Array.Reverse(cadena);
+        return (texto);
+    }
+}
+
+
+
+
+
